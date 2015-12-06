dd0ca5399ebfbac8b07c529e5f07c0b50d3471b8
Rafal Wolak <wolak.rafal@gmail.com>
2015-12-06 19:07:39 +0100

    Monad Lab: mapM, filterM, foldLeftM, foldRightM, liftM

--- 2015/src/MonadLab.hs
+++ 2015/src/MonadLab.hs
@@ -48,8 +48,91 @@ sequence2 ms        = foldr func (return []) ms
                           func m acc = do x <- m
                                           xs <- acc
                                           return (x : xs)
--- akes a non-bottom function of type a -> m b, and a finite, non-partial list of non-bottom elements of type a
+-- Takes a non-bottom function of type a -> m b, and a finite, non-partial list of non-bottom elements of type a
 -- and (similarly to map) applies the function to every element of the list, but produces the resulting list
 -- wrapped inside a monadic action
 -- Note:mapM' must preserve the order of the elements of the input list.
+foo prompt = do putStr' prompt
+                x <- getLine
+                return x
+
 mapM' :: Monad m => (a -> m b) -> [a] -> m [b]
+mapM' f as = sequence' (map f as)
+
+mapM2 f [] = return []
+mapM2 f (a : as) = f a >>= \b -> mapM' f as >>= \bs -> return (b : bs)
+
+mapM3 f [] = return []
+mapM3 f (a : as) = do b <- f a
+                      bs <- mapM3 f as
+                      return (b : bs)
+
+mapM4 f [] = return []
+mapM4 f (a : as) = f a >>=
+                    \b -> do
+                            bs <- mapM4 f as
+                            return (b : bs)
+
+
+
+
+-- The following definitions implements the function
+-- filterM' :: Monad m => (a -> m Bool) -> [a] -> m [a],
+-- that takes a "predicate" of type Monad m => a -> m Bool
+-- and uses this to filter a finite, non-partial list of non-bottom elements of type a.
+--          Note: filterM' must preserve the order of the elements of
+--                the input list, as far as they appear in the result.
+
+ask t = do putStr' ("Is upper: " ++ t)
+           res <- getLine
+           if (res == "y") then return True else return False
+
+filterM' :: Monad m => (a -> m Bool) -> [a] -> m [a]
+filterM' _ []       = return []
+filterM' p (x : xs) = do flag <- p x
+                         ys <- filterM' p xs
+                         if flag then return (x : ys) else return ys
+
+
+
+--Function that takes an accumulation function a -> b -> m a,
+-- and a seed of type a and left folds a finite,
+-- non-partial list of non-bottom elements of type b into a single result of type m a
+foldLeftM            :: Monad m => (a -> b -> m a) -> a -> [b] -> m a
+foldLeftM _ a []     = return a
+foldLeftM f a (b:bs) = do aa <- f a b
+                          foldLeftM f aa bs
+
+-- > foldLeftM (\a b -> putChar b >> return (b : a ++ [b])) [] "haskell" >>= \r -> putStrLn r
+-- result: haskelllleksahhaskell
+
+
+-- Hint: The recursive structure of foldLeftM looks as follows:
+--
+-- foldLeftM f a [] = ... a ...
+-- foldLeftM f a (x:xs) = foldLeftM f (... f ... a ... (>>=) ...) xs
+-- Remember the definition of foldl:
+--
+-- foldl f a [] = ... a ...
+-- foldl f a (x:xs) = foldl f (... f ... a ...) xs
+-- What is the result of evaluating the expression:
+
+-- implementation of foldRightM
+foldRightM              :: Monad m => (a -> b -> m b) -> b -> [a] -> m b
+foldRightM _ b []       = return b
+foldRightM f b (a : as) = do bb <- foldRightM f b as
+                             f a bb
+
+-- > foldRightM (\a b -> putChar a >> return(a:b)) [] (show [1,3..10]) >>= \r -> putStrLn r
+-- result : ]9,7,5,3,1[[1,3,5,7,9]
+
+
+-- liftM all possible implementations that define a function
+-- liftM :: Monad m => (a -> b) -> m a -> m b
+-- that takes a function of type a -> b and "maps" it over a
+-- non-bottom monadic value of type m a to produce a value of type m b
+liftM :: Monad m => (a -> b) -> m a -> m b
+liftM f m = do a <- m
+               return (f a)
+
+liftM' f m = m >>= \a -> return (f a)
